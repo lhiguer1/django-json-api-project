8516fba9d526c15f911ca2a5e01054b581eef874
Leonel Higuera <lhiguer1@asu.edu>
2022-04-24 23:35:18 -0700

    cache breakingbadapi.com calls

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.core.handlers.wsgi import WSGIRequest
 from requests import get
+from django.core.cache import cache
 
 BREAKING_BAD_API_BASE_URL = 'https://www.breakingbadapi.com/api/'
 
@@ -9,11 +10,15 @@ def ping(request: WSGIRequest):
     return JsonResponse({'success': True}, status=200)
 
 def get_characters_from_api(name) -> list:
-    resp = get(BREAKING_BAD_API_BASE_URL+'characters', params={'name': name})
-    return resp.json() # list of characters
+    characters = cache.get(name)
+    if not characters:
+        resp = get(BREAKING_BAD_API_BASE_URL+'characters', params={'name': name})
+        characters = resp.json()
+        cache.set(name, characters, timeout=60*5)
+    return characters # list of characters
 
-def parse_names(names_string):
-    return list({name.strip() for name in names_string.split(',') if name})
+def parse_names(names_string:str):
+    return list({name.strip().lower() for name in names_string.split(',') if name})
 
 def characters(request: WSGIRequest):
     names_query = request.GET.get('names')
